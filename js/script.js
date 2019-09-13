6ac19281b8ae67d10d1fa52e1251cc52ab010230
matuzalema <matuzalema@gmail.com>
2019-09-13 16:02:56 +0200

    Add footer

--- js/script.js
+++ js/script.js
@@ -1,58 +1,55 @@
 /*----------Animate items-----------*/
 window.sr = ScrollReveal();
-sr.reveal('.first-header', {
-    delay: 500,
-    duration: 2000,
-    mobile: true
+sr.reveal(".first-header", {
+  delay: 500,
+  duration: 2000,
+  mobile: true
 });
 
-sr.reveal('.header-desc', {
-    delay: 2000,
-    mobile: true,
-    duration: 2000
+sr.reveal(".header-desc", {
+  delay: 2000,
+  mobile: true,
+  duration: 2000
 });
 
-sr.reveal('.fa-comments', {
-    delay: 1000,
-    mobile: true,
-    duration: 2000
+sr.reveal(".fa-comments", {
+  delay: 1000,
+  mobile: true,
+  duration: 2000
 });
 
-sr.reveal('.project-links', {
-    delay: 1000,
-    mobile: true,
-    duration: 2000
+sr.reveal(".project-links", {
+  delay: 1000,
+  mobile: true,
+  duration: 2000
 });
 
-sr.reveal('.about-desc', {
-    delay: 1000,
-    mobile: true,
-    duration: 2000
+sr.reveal(".about-desc", {
+  delay: 1000,
+  mobile: true,
+  duration: 2000
 });
 
-
-
-sr.reveal('input', {
-    delay: 1000,
-    mobile: true,
-    duration: 2000
+sr.reveal("input", {
+  delay: 1000,
+  mobile: true,
+  duration: 2000
 });
 
-sr.reveal('textarea', {
-    delay: 500,
-    mobile: true,
-    duration: 2000
+sr.reveal("textarea", {
+  delay: 500,
+  mobile: true,
+  duration: 2000
 });
 
-sr.reveal('.fa-angle-double-down', {
-    delay: 3000,
-    mobile: true,
-    duration: 4000
+sr.reveal(".fa-angle-double-down", {
+  delay: 3000,
+  mobile: true,
+  duration: 4000
 });
 
-sr.reveal('.hexagon', {
-    delay: 2000,
-    mobile: true,
-    duration: 2000
+sr.reveal(".hexagon", {
+  delay: 1500,
+  mobile: true,
+  duration: 2000
 });
-
